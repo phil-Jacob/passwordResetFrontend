60427cbab1b7eee9adffdd62ac703c4af53f64e0
phil-Jacob <133244505+phil-Jacob@users.noreply.github.com>
2024-01-31 00:00:20 +0530

    Update signup.component.js

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -16,7 +16,7 @@ export default class SignUp extends Component {
     e.preventDefault();
     const { fname, lname, email, password} = this.state;
     console.log(fname, lname, email, password);
-     fetch ("http://localhost:5000/register",{
+     fetch ("https://password-resetbackend.onrender.com/register",{
       method:"POST",
       crossdomian:true,
       headers:{
