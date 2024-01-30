51de15cee2382cc114c6afa2d5f453437a71eba7
phil-Jacob <133244505+phil-Jacob@users.noreply.github.com>
2024-01-30 23:59:25 +0530

    Update login.component.js

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -13,7 +13,7 @@ export default class Login extends Component {
     e.preventDefault();
     const { email, password} = this.state;
     console.log( email, password);
-    fetch("http://localhost:5000/login-user",{
+    fetch("https://password-resetbackend.onrender.com/login-user",{
       method:"POST",
       crossdomian:true,
       headers:{
